918ba412f6f41c71567d5be84b5da4630f8cef07
Serge Cage <sergeycage@rocketmail.com>
2023-10-05 19:08:58 +0300

    feat: solve the task Thu: 05.10.23

--- src/index.js
+++ src/index.js
@@ -37,8 +37,15 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+const decode =(expr) => {
+    let arr = [];
+
+    for (let i = 0; i < expr.length/10; i++) {
+        let j = i * 10;
+        arr.push(MORSE_TABLE[expr.slice(j, j + 10).replace(/10/g, '.').replace(/10/g, '.')
+        .replace(/11/g, '-').replace(/00/g, '')] || ' ');
+    }
+    return arr.join('');
 }
 
 module.exports = {
